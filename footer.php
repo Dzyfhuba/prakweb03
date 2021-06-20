42bad70a526e6685fa198f171408241b07e4390d
Dzyfhuba <hafidz21ub@gmail.com>
2021-06-20 22:27:50 +0700

    revision 2

---
+++ footer.php
@@ -0,0 +1,15 @@
+<!-- Copyright -->
+<!-- <div class="footer-copyright text-center p-0 fixed-bottom">
+	Hafidz Ubaidillah
+    <a class="text-decoration-none" href="https://github.com/Dzyfhuba/prakweb02.git">Github Repository</a>
+</div> -->
+<!-- Copyright -->
+
+<footer class="fixed-bottom text-white-50" style="background-color: #343a40;">
+    <div class="footer-copyright text-center py-3">Source:
+        <a href="https://github.com/Dzyfhuba/prakweb02.git/"> Dzyfhuba's Github Repository</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
--- home/index.php
+++ home/index.php
@@ -13,5 +13,6 @@
         <h2>Selamat Datang di Website Saya</h2>
         <h4>Hafidz Ubaidillah - 190602043</h4>
     </div>
+    <?php include "../footer.php" ?>
 </body>
 </html>
